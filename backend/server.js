7602705c45a0af4fa734db4fe5b46175b481e60b
22078802 <amaokyof03@gmail.com>
2026-04-20 10:26:06 +0000

    fix render deployment port binding

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,6 @@ app.use("/api/user", userRoutes)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`)
 })
