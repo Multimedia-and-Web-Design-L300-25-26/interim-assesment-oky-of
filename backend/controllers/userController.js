f06b9a520fb1e2c5ddbf2b38d909de2e01d1f76c
22078802 <amaokyof03@gmail.com>
2026-04-27 15:10:17 +0000

    add all changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 import User from "../models/user.js";
 
+export const getUsers = async (req, res) => {
+  const users = await User.find().select("-password").sort({ createdAt: -1 });
+  res.json(users);
+};
+
 export const getProfile = async (req, res) => {
   const user = await User.findById(req.user.id).select("-password");
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProfile } from "../controllers/userController.js";
+import { getProfile, getUsers } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.get("/", protect, getUsers);
 router.get("/profile", protect, getProfile);
 
 export default router;
